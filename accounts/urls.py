5b12d597c134af6096d1874a44afcb08d9321dd8
Ke Liu <lk5998800@gmail.com>
2024-09-12 03:47:47 -0400

    change comments to English for my son

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,19 +2,19 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
 
-
 app_name = 'accounts'
 
 urlpatterns = [
 
-
-    # 注册页面
+    # Registration page
     path('register/', views.register, name='register'),
 
-    # 登录页面，使用Django内置的LoginView
+    # Login page, using Django's built-in LoginView
     path('login/', views.user_login, name='login'),
 
-    path('img_captcha/', views.img_captcha, name='img_captcha'),  # 验证码的 URL 路由
-    # 退出登录页面
+    # URL route for captcha
+    path('img_captcha/', views.img_captcha, name='img_captcha'),
+
+    # Logout page
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -11,10 +11,10 @@ from django.http import HttpResponse
 from captcha.image import ImageCaptcha
 
 
-# 短信发送函数
+# SMS sending function
 def send_sms(telephone, sms_code):
-    # 在这里使用第三方API发送短信验证码
-    # 比如阿里云、腾讯云短信服务等
+    # Use third-party API to send SMS verification code here
+    # For example, Aliyun, Tencent Cloud SMS services, etc.
     pass
 
 
@@ -22,27 +22,27 @@ def register(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            # 短信验证码和图形验证码验证通过
-            # 创建用户并保存
+            # SMS verification code and image captcha passed
+            # Create user and save
             username = form.cleaned_data['username']
             password1 = form.cleaned_data['password1']
             password2 = form.cleaned_data['password2']
             email = form.cleaned_data['email']
             sex = form.cleaned_data['sex']
-            if password1 != password2:  # 判断两次密码是否相同
-                message = "两次输入的密码不同！"
+            if password1 != password2:  # Check if the two passwords match
+                message = "The two passwords entered are different!"
                 return render(request, 'user19/register.html', locals())
             else:
                 same_name_user = User.objects.filter(name=username)
-                if same_name_user:  # 用户名唯一
-                    message = '用户已经存在，请重新选择用户名！'
+                if same_name_user:  # Username must be unique
+                    message = 'User already exists, please choose a different username!'
                     return render(request, 'user19/register.html', locals())
                 same_email_user = User.objects.filter(email=email)
-                if same_email_user:  # 邮箱地址唯一
-                    message = '该邮箱地址已被注册，请使用别的邮箱！'
+                if same_email_user:  # Email must be unique
+                    message = 'This email address has already been registered, please use a different email!'
                     return render(request, 'user19/register.html', locals())
 
-                # 当一切都OK的情况下，创建新用户
+                # When everything is OK, create a new user
 
                 new_user = User.objects.create()
                 new_user.name = username
@@ -51,23 +51,23 @@ def register(request):
                 new_user.sex = sex
                 new_user.save()
 
-            # 自动登录新注册的用户
+            # Automatically log in the newly registered user
             user_login(request)
-            return redirect('main')  # 重定向到主页
+            return redirect('main')  # Redirect to homepage
         else:
-            messages.info(request, '输入格式错误')
+            messages.info(request, 'Invalid input format')
             return render(request, 'accounts/register.html', {'form': form})
 
     else:
         form = RegisterForm()
 
-        # 如果是 GET 请求，生成并发送短信验证码
+        # If it is a GET request, generate and send an SMS verification code
         if 'telephone' in request.GET:
             phone_number = request.GET['telephone']
-            sms_code = str(random.randint(100000, 999999))  # 生成 6 位随机验证码
-            request.session['sms_captcha'] = sms_code  # 将验证码存储在 session 中
-            send_sms(phone_number, sms_code)  # 调用发送短信的函数
-            messages.info(request, '短信验证码已发送到您的手机。')
+            sms_code = str(random.randint(100000, 999999))  # Generate a 6-digit random verification code
+            request.session['sms_captcha'] = sms_code  # Store the verification code in the session
+            send_sms(phone_number, sms_code)  # Call the SMS sending function
+            messages.info(request, 'The SMS verification code has been sent to your phone.')
 
     return render(request, 'accounts/register.html', {'form': form})
 
@@ -93,8 +93,8 @@ def delete_user(username):
 
 
 def img_captcha(request):
-    image = ImageCaptcha()  # 使用 ImageCaptcha 类生成验证码
-    data = image.generate('1234')  # 这里你可以生成一个随机的验证码字符串
+    image = ImageCaptcha()  # Use the ImageCaptcha class to generate a captcha
+    data = image.generate('1234')  # You can generate a random captcha string here
     response = HttpResponse(data, content_type='image/png')
     return response
 
@@ -105,7 +105,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 
-# 登录视图
+# Login view
 def user_login(request):
     if request.method == 'POST':
         form = UserForm(request, request.POST)
@@ -115,20 +115,20 @@ def user_login(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.success(request, f'欢迎回来, {username}!')
-                return redirect('main')  # 登录后重定向到主页
+                messages.success(request, f'Welcome back, {username}!')
+                return redirect('main')  # Redirect to homepage after login
             else:
-                messages.error(request, '用户名或密码不正确')
+                messages.error(request, 'Incorrect username or password')
         else:
-            messages.error(request, '表单无效，请重新尝试')
+            messages.error(request, 'Invalid form, please try again')
     else:
         form = UserForm()
 
     return render(request, 'accounts/login.html', {'form': form})
 
 
-# 注销视图
+# Logout view
 def user_logout(request):
     logout(request)
-    messages.success(request, '您已成功注销')
-    return redirect('accounts:login')  # 注销后重定向到登录页面
+    messages.success(request, 'You have successfully logged out')
+    return redirect('accounts:login')  # Redirect to login page after logout
